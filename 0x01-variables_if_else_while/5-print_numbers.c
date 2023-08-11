14d8a496fb483c2f63d7b5c5d51706eb4b8e1d57
AadelAferyad <animevineu@gmail.com>
2023-08-11 20:24:19 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ * main : Enrty point
+ * return: Return always 0
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
