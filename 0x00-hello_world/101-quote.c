5a3eb1a1e169f0c80fdf27e07101359e57284c97
AadelAferyad <animevineu@gmail.com>
2023-08-11 01:05:18 +0100

    unistd write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main The Entry point
+ *
+ * write: print the string to stdout
+ * Return: Always 1
+*/
+int main(void)
+{
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
+	return (1);
+}
