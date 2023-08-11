44b4cb11740bd689f900cac98949eec4d67a1006
AadelAferyad <animevineu@gmail.com>
2023-08-11 19:34:51 +0100

    Print both uppercase and lowercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putchar('a' + i);
+	for (i = 0; i < 26; i++)
+		putchar('A' + i);
+	putchar('\n');
+	return (0);
+}
