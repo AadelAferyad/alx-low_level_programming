7c4688e8c055b5b24aac5c520fb81be2c60bf158
AadelAferyad <animevineu@gmail.com>
2023-08-15 22:31:32 +0100

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of
+ * Jack Bauer, starting from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
