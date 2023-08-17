c370aef66163a211d6e296c704781739cb2ccd92
AadelAferyad <animevineu@gmail.com>
2023-08-17 20:24:41 +0100

    print the most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print digits 0 - 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (48 + i == 50 || 48 + i == 52)
+		{
+			continue;
+		}
+		else
+			_putchar(48 + i);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,4 +7,5 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
+void print_most_numbers(void);
 #endif /* MAIN_H */
