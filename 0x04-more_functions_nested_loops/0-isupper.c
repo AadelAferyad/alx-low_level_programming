42d4c8c398fbb54c54a838cf8c1a540109e902e5
AadelAferyad <animevineu@gmail.com>
2023-08-17 19:34:35 +0100

    Is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a char is uppercase
+ * @c: ascci value
+ * Return: largest number
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+}
