ef86c0ec5b701200d63516e4999c12d80dcd2cef
AadelAferyad <animevineu@gmail.com>
2023-08-17 19:48:14 +0100

    check if a ascii value represent a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if a inputed value is a digit
+ * @c: ascci value
+ * Return: 1 if it's number if not return 0
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 58)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+
 int _isupper(int c);
+int _isdigit(int c);
 #endif /* MAIN_H */
