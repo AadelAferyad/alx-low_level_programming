c7b40d098c4b6c2ea4eddb40dac66083cc6d3a05
AadelAferyad <animevineu@gmail.com>
2023-08-18 00:30:11 +0100

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,21 +7,25 @@
 
 void print_triangle(int size)
 {
-	int i, j, k;
-
 	if (size <= 0)
-		_putchar('\n');
-
-	for (i = 0; i <= size; i++)
 	{
-		for (j = size; j > i; j--)
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j, k;
+
+		for (i = 0; i <= size; i++)
 		{
-			_putchar(' ');
-		}
-		for (k = 1; k <= i; k++)
-		{
-			_putchar('#');
-		}
+			for (j = i; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
 			_putchar('\n');
+		}
 	}
 }
