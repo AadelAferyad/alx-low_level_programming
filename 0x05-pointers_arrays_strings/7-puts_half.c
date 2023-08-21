1b59ef5c76a6d08c8e9170e56c8cd7be8862bb3e
AadelAferyad <animevineu@gmail.com>
2023-08-22 00:40:52 +0100

    print only half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,21 +7,19 @@
 
 void puts_half(char *str)
 {
-	int i = 0;
-	int j = 0;
-	int n = 0;
+	int a, n, i;
 
-	while (str[i] != '\n')
-	{
+	i = 0;
+
+	while (str[i] != '\0')
 		i++;
-	}
-	n = i / 2; 
-	if (i % 2 == 1)
-		n = (i -1)/2;
-	while (n <= i)
-	{
-		_putchar(str[n]);
-		n++;
-	}
+
+	n = (i / 2);
+
+	if ((i % 2) == 1)
+		n = ((i - 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
 	_putchar('\n');
 }
