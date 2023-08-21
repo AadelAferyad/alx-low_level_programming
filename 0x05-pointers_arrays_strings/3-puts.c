0bb4ec263fdb889ab43ae6bbdea5b93639751ce5
AadelAferyad <animevineu@gmail.com>
2023-08-21 18:31:09 +0100

    print to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints the string to stdout
+ * @str: pointer to char
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		_putchar(s[i]);
+	_putchar('\n');
+}
