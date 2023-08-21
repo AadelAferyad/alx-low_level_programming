8801c2cb7c79518dac5bf07a55ceb78149bdbe9d
AadelAferyad <animevineu@gmail.com>
2023-08-21 19:51:08 +0100

    reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: pointer to char
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (j < i)
+	{
+		temp = s[i];
+		s[j] = s[i];
+		s[i] = temp;
+		j++;
+		i--;
+	}
+}
