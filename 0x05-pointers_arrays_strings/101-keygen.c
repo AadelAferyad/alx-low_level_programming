0443a359a947e9a18a3746f1df29595fd72f2b90
AadelAferyad <animevineu@gmail.com>
2023-08-21 22:32:43 +0100

    genrate random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Generates random passwords 101-crackme
+ *
+ * Return: Always 0
+ */
 int main(void) 
 {
-    srand(time(NULL)); 
 
-    int random, sum=0;
+    int sum=0;
+    char random;
+    
+    srand(time(NULL)); 
     while(sum <= 2772)
     {
-        random=rand()%128;
-        sum+=random;
+        random= rand() % 128;
+        sum += random;
         putchar(random);
     }
     putchar(2772 - sum);
 
-    return 0;
+    return (0);
 }
