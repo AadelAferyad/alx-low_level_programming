8abdf1c047088ae341db88c620e7a0e1f96497f8
AadelAferyad <animevineu@gmail.com>
2023-08-23 16:41:37 +0100

    strncopy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+	* _strncpy - coppy a string
+	* @dest: pointer to char, store the copied string
+	* @src: pointer to char,  the string we want to coppy
+	* @n: number of bytes you want to coppy
+	* Return: return the address after adding the string to it
+	*/
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
