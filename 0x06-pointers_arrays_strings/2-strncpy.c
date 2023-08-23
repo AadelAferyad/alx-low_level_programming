0df49c54e1e09890161cea0cb718cf12863dd27e
AadelAferyad <animevineu@gmail.com>
2023-08-23 16:52:44 +0100

    strncopy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 	* _strncpy - coppy a string
@@ -18,5 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i++;
 	}
+	if (i < n)
+		dest[i] = '\0';
 	return (dest);
 }
