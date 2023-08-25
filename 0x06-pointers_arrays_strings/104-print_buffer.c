753d96071fb43d296f9f7c0b405c886264435920
AadelAferyad <animevineu@gmail.com>
2023-08-25 02:49:16 +0100

    print buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ *
+ */
+void print_buffer(char *b, int size)
+{
+	int i = 0, j = 0, k = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (k < size)
+	{
+		j = size - k < 10 ? size - k : 10;
+		printf("%08x: ", k);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + k + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + k + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		k += 10;
+	}
+}
