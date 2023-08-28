48c6946e5b11ce70f80cf962a795307a766ceb77
AadelAferyad <animevineu@gmail.com>
2023-08-28 18:19:45 +0100

    Task-2 _strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,11 @@
 
 char *_strchr(char *s, char c)
 {
-		int i = 0;
-
-		while (s[i])
+		while (*s)
 		{
-			if (s[i] == c)
-				return (s + i);
-			i++;
+			if (*s == c)
+				return (s);
+			s++;
 		}
 
 		return (NULL);
