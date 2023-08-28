f30a8aa2b3ba09c918eaa96956c6f440a3c3403a
AadelAferyad <animevineu@gmail.com>
2023-08-28 17:52:04 +0100

    Task-1 _memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *	_memcpy - copies memory from a buffer
+ *	@dest: pointer to char the buffer we want to fill with new memory
+ *	@src: pointer to char the buffer memory will be taken from
+ *	@n: size of memory will be taken
+ *	Return: return buffer with new chunks on memory
+ */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+		int i = 0;
+
+		while (i < n)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+
+		return (dest);
+}
