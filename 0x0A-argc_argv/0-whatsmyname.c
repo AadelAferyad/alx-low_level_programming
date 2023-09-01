623bd2bec70c84b3b1b39da1f99132a674fde14e
AadelAferyad <animevineu@gmail.com>
2023-09-01 12:47:50 +0100

    Task-0 Print program's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ *
+ * @argc: integer argument count.
+ * @argv: integer argument vector.
+ *
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(int argc, char **argv)
+{
+		printf("%s\n", *argv);
+		return (0);
+}
