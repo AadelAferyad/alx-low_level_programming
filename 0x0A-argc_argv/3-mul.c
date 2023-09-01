7d2174cfaffac9c75ceda69bb82bd6cbfd4ddfc8
AadelAferyad <animevineu@gmail.com>
2023-09-01 18:10:45 +0100

    Task-3 multi of two integers passed in as argument also atoi fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,6 +10,7 @@
  *
  * Return: Always 0 (Success).
  */
+
 int main(int argc, char **argv)
 {
 	if (argc <= 2)
@@ -19,7 +20,7 @@ int main(int argc, char **argv)
 	}
 	if (atoi(argv[1]) != 0 && atoi(argv[2]) != 0)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[1]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 
 	return (0);
