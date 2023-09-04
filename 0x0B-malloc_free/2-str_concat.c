65c0c546068a0991b8d540768146e7fb61269c78
AadelAferyad <animevineu@gmail.com>
2023-09-04 23:55:15 +0100

    Task-2 concat on the heap

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,53 +2,6 @@
 
 
 /**
- * _strlen - length of a string.
- *
- * @s: string.
- *
- * Return: return length of string.
- */
-
-unsigned int _strlen(char *s)
-{
-	unsigned int i = 0;
-
-	if (s == NULL)
-		return (0);
-	while (s[i])
-		i++;
-	return (i);
-}
-
-/**
- * _strcat - function that concat a 2 strings.
- *
- * @dest: first string.
- * @src: second string.
- *
- * Return: return concatenated string.
- */
-
-char *_strcat(char *dest, char *src)
-{
-	int i, j = 0;
-
-	i = _strlen(dest);
-	if (dest != NULL && src == NULL)
-		return (dest);
-	else if ((dest == NULL && src != NULL) || (dest != NULL && src != NULL))
-	{
-		while (src[j])
-		{
-			dest[i] = src[j];
-			i++;
-			j++;
-		}
-		dest[i] = '\0';
-	}
-	return (dest);
-}
-/**
  * str_concat - function concatenate strings in the heap.
  *
  * @s1: first string.
@@ -57,34 +10,44 @@ char *_strcat(char *dest, char *src)
  * Return: return allocated memory.
  */
 
+
 char *str_concat(char *s1, char *s2)
 {
-	char *p;
-	int i = 0, j = 0;
+	int s_1 = 0, s_2 = 0, i = 0;
+	char *buffer;
 
-	i = _strlen(s1) + _strlen(s2);
-	p = (char *) malloc(i + 1);
-	if (p == NULL)
+	while (s1[s_1])
+		s_1++;
+	while (s2[s_2])
+		s_2++;
+	buffer = (char *) malloc(s_1 + s_2 + 1);
+	if (buffer == NULL)
 		return (NULL);
-	if (s1 != NULL)
-		{
-		while (s1[j])
-		{
-			p[j] = s1[j];
-			j++;
-		}
-		p = _strcat(p, s2);
-	}
-	else if (s2 != NULL)
+	while (i < s_1 + s_2)
 	{
-		while (s2[j])
+		if (i < s_1)
 		{
-			p[j] = s2[j];
-			j++;
+			buffer[i] = s1[i];
 		}
-		p[j] = '\0';
+		else
+			buffer[i] = s2[i - s_1];
+
+		i++;
 	}
-	else
-		return (0);
-	return (p);
+	return (buffer);
+}
+
+int main(void)
+{
+    char *s;
+
+    s = str_concat("Betty ", "Holberton");
+    if (s == NULL)
+    {
+        printf("failed\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
 }
