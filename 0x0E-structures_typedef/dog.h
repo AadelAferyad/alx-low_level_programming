a67231d721cb72397fe0bc9022568e928cad226e
AadelAferyad <animevineu@gmail.com>
2023-09-11 18:41:20 +0100

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,8 +19,8 @@ struct dog
 
 };
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* _HEADER_ */
