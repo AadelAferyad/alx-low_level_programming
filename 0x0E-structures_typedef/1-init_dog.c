a0fc44a33856d39b722dde50c198b3d9db9db505
AadelAferyad <animevineu@gmail.com>
2023-09-11 14:37:41 +0100

    Task-1 init the doggy struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,13 +7,16 @@
  * @name: pointer to char string dog's name.
  * @age: float the age dog's age.
  * @owner: pointer to char string dog's owner.
- *
+ * Return: void
  */
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
