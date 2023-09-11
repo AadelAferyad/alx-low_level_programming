d641ba0eba18412d4f75a96a9bdc72a7b2703b24
AadelAferyad <animevineu@gmail.com>
2023-09-11 18:34:52 +0100

    Task-4 add new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,35 @@
 #include "dog.h"
 #include <stdlib.h>
 
+
+/**
+ * _strdup - function that allocates a block of memory and initialize it.
+ *
+ * @str: string will be coppied to the allocated memory.
+ *
+ * Return: return allocated memory.
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *p;
+
+	while (str[i])
+		i++;
+	p = (char *) malloc(i + 1);
+	if (p == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		p[i] = str[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
 /**
  * new_dog - creates a new dog.
  * @name: name of the dog.
@@ -17,9 +46,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
+	(*d).name = _strdup(name);
+	(*d).owner = _strdup(owner);
+	if ((*d).name == NULL || (*d).owner == NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+		return (NULL);
+	}
+
 	d->age = age;
-	d->owner = owner;
 
 	return (d);
 }
