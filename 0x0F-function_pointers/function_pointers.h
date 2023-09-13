14ab6c470fb437ab5cefa454af714ee9153dddaf
AadelAferyad <animevineu@gmail.com>
2023-09-13 17:18:53 +0100

    fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef _HEADER_
 #define _HEADER_
-#include <stdlib.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
