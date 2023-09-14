5d3c137afa21da8e3cd2d0d73aaa9eb46d08313d
AadelAferyad <animevineu@gmail.com>
2023-09-14 10:41:29 +0100

    Task-1 print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+
+/**
+ * print_numbers - function that returns the sum of all its parameters.
+ *
+ * @separator: pointer to char the symbole will separate numbers.
+ * @n: The number of parameter fun will take.
+ *
+*/
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list pa;
+
+	if (n == 0)
+		return;
+	va_start(pa, n);
+	while ((i++) < n)
+	{
+		if (separator == NULL)
+			printf("%d", va_arg(pa, int));
+		else
+			printf("%d", va_arg(pa, int));
+		if (separator != NULL && i <= n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(pa);
+}
