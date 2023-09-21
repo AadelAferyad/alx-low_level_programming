9668c33123524d812561d8c61fc4b1639dd7e428
AadelAferyad <animevineu@gmail.com>
2023-09-21 16:51:43 +0100

    Count number of nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - count number of nodes.
+ *
+ * @h: pointer to linked list or node.
+ *
+ * Return: return number on nodes.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
