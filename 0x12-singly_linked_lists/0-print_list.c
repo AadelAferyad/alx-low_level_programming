f73a72916a0693c04e810eda4f0af21c06d60730
AadelAferyad <animevineu@gmail.com>
2023-09-21 16:33:58 +0100

    Print and return the number of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - print nodes elements.
+ *
+ * @h: pointer to struct singly linked list.
+ *
+ *
+ * Return: return number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *local_head;
+	size_t n_element = 0;
+	int i = 0;
+
+	local_head = h;
+	while (local_head != NULL)
+	{
+		if (!local_head->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", local_head->len, local_head->str);
+		n_element++;
+		local_head = local_head->next;
+	}
+	return (n_element);
+}
