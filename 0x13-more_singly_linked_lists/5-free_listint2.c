86b055b05035b154bde0737c5daae1b9635cd958
AadelAferyad <animevineu@gmail.com>
2023-09-25 21:31:33 +0100

    Free all the allocated nodes plus set the head to point to null plus fixed some bugs

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,11 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	if (head == NULL || *head == NULL)
 		return;
+	temp = *head;
 	while (temp)
 	{
 		temp = (*head)->next;
