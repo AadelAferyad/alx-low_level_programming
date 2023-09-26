d250de6360eea11f9ab9b09bbdf11ecf57acb2fd
AadelAferyad <animevineu@gmail.com>
2023-09-26 18:00:31 +0100

    Insert at the Nth index, back to back

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!node)
 		return (NULL);
 	node->n = n;
-	if (*head == NULL || idx == 0)
+	if ((*head == NULL && idx == 0) || idx == 0)
 	{
 		node->next = *head;
 		*head = node;
