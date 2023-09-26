94320c1f214e75604c2b63de9da24c4a33013800
AadelAferyad <animevineu@gmail.com>
2023-09-26 17:39:40 +0100

    Insert at the Nth index, different approache

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,11 +35,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			node->next = temp;
 			break;
 		}
-		else if (temp->next == NULL)
-		{
-			temp->next = node;
-			node->next = NULL;
-		}
 		i++;
 		prev = temp;
 		temp = temp->next;
