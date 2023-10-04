b019347877bd8ff816f68378b3b1bc7a1225968f
AadelAferyad <animevineu@gmail.com>
2023-10-04 02:50:21 +0100

    allocate a 2d array of integers initilazed with 0

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	array = (int **) malloc(sizeof(int *) * height);
 	if (!array)
 		return (NULL);
-	while (i <  width)
+	while (i <  height)
 	{
 		array[i] = (int *) malloc(sizeof(int) * width);
 		if (!array[i])
