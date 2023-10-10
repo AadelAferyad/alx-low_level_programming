6a3d323a2b81a96938d8aaa80b23255eba08fe0d
AadelAferyad <animevineu@gmail.com>
2023-10-10 23:58:51 +0100

    cute

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,7 @@ struct dog
 	char *owner;
 
 };
-typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
