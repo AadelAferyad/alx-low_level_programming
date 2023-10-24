a0388dbf7d35a1c39649aa24bec78497559ca6d0
AadelAferyad <animevineu@gmail.com>
2023-10-24 23:27:57 +0100

    add node at the nth index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,6 +43,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head == NULL && idx == 0)
 	{
 		*head = node;
+		node->next = *head;
 		return (node);
 	}
 	len = _strnode(*head);
