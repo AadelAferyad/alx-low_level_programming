ffdc2072fd1949f2fe455a7fb4c196770eb67452
AadelAferyad <animevineu@gmail.com>
2023-10-24 23:09:57 +0100

    add node at the nth index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+
+/**
+ * _strnode - calcul the number of nodes.
+ *
+ * @h: pointer to the head.
+ *
+ * Return: length.
+ *
+ */
+unsigned int _strnode(listint_t *h)
+{
+	unsigned int len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
+/**
+ * insert_nodeint_at_index - insert at the index.
+ *
+ * @head: double pointer to the head.
+ * @idx: index.
+ * @n: data.
+ * Return: new node.
+ *
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *position, *prev;
+	unsigned int len;
+
+	if (!head)
+		return (NULL);
+	node = malloc(sizeof(listint_t));
+	if (!node)
+		return (NULL);
+	node->n = n;
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+	len = _strnode(*head);
+	if (idx > len)
+	{
+		free(node);
+		return (NULL);
+	}
+	for (len = 0, position = *head; position; len++)
+	{
+		if (len == idx)/*0-->0-->0--->0-->*/
+		{
+			prev->next = node;
+			node->next = position;
+			break;
+		}
+		prev = position;
+		position = position->next;
+	}
+
+	return (node);
+}
