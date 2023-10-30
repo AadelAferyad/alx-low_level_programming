8c28fa5e3f3d67cb20acb6037e1a97032e7ebc1b
AadelAferyad <animevineu@gmail.com>
2023-10-30 22:56:49 +0100

    File

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlean - strlen calculate the length.
+ * @s: string.
+ * Return: length.
+ *
+ */
+int _strlean(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+/**
+ * create_file - function that creates a file.
+ *
+ * @filename: file that will be oppend.
+ * @text_content: fill the file with this content.
+ * Return: 1 if success and -1 if failed.
+ *
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int len, fp;
+	ssize_t write_bytes;
+
+	if (!filename)
+		return (-1);
+	fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fp == -1)
+		return (-1);
+	len = _strlean(text_content);
+	write_bytes = write(fp, text_content, len);
+	if (write_bytes == -1)
+	{
+		close(fp);
+		return (-1);
+	}
+	close(fp);
+	return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlean - strlen calculate the length.
+ * @s: string.
+ * Return: length.
+ *
+ */
+int _strlean(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+/**
+ * append_text_to_file - function that appends text at the end of a file.
+ *
+ * @filename: file that will be oppend.
+ * @text_content: the content will be appended to the file.
+ * Return: 1 if success and -1 if failed.
+ *
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fp, read_bytes, len;
+
+	if (!filename)
+		return (-1);
+	fp = open(filename, O_WRONLY | O_APPEND);
+	if (fp == -1)
+		return (-1);
+	len = _strlean(text_content);
+	read_bytes = write(fp, text_content, len);
+	if (read_bytes == -1)
+	{
+		close(fp);
+		return (-1);
+	}
+	close(fp);
+	return (1);
+}
