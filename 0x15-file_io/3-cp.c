f2b6a16e093ee334e746100be15a268b2b32da6a
AadelAferyad <animevineu@gmail.com>
2023-10-31 01:06:05 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,12 +34,12 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	file_from = open(av[1], O_RDONLY);
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((read_bytes = read(file_from, buffer, 1024)) > 0)
 	{
 		write_bytes = write(file_to, buffer, read_bytes);
