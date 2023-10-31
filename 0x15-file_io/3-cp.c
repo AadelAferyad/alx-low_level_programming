9010a4e3dd43222c3ed21d79d09aaa93fa65173c
AadelAferyad <animevineu@gmail.com>
2023-10-31 01:30:10 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,8 +13,7 @@ void close_err(int file_from, int file_to)
 	}
 	if (close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to),exit(100);
 	}
 }
 /**
@@ -29,31 +28,21 @@ int main(int ac, char **av)
 	char buffer[1024];
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(av[1], O_RDONLY);
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((read_bytes = read(file_from, buffer, 1024)) > 0)
 	{
 		write_bytes = write(file_to, buffer, read_bytes);
 		if (read_bytes != write_bytes)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
+	if (read_bytes == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	if (file_to == -1 || write_bytes == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	close_err(file_from, file_to);
 	return (0);
 }
