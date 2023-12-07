2254dcb2d8d019ba6e60095f2cf11d14e4e070aa
AadelAferyad <animevineu@gmail.com>
2023-12-08 00:33:49 +0100

    delete node at the nth node

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete node at the nth index.
+ *
+ * @head: pointer to pointer to first node.
+ * @index: the nth index.
+ *
+ * Return: 1 if it succeeded, -1 if it failed.
+ *
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head, *tmp = *head;
+	unsigned int i = 0;
+
+	if (!head || !(*head))
+		return (-1);
+	if (index == 0 && *head)
+	{
+		if ((*head)->next)
+		{
+			tmp = (*head)->next, free(*head);
+			*head = tmp;
+			(*head)->prev = NULL;
+		}
+		else
+		{
+			free(*head);
+			*head = NULL;
+		}
+		return (1);
+	}
+	while (current->next)/*N<-0->0<-0->n*/
+	{
+		if (i == index && current->next)
+		{
+			tmp = current, current->next->prev = current->prev;
+			current->prev->next = current->next, free(tmp);
+			return (1);
+		}
+		i++, current = current->next;
+	}
+	if (++i == index)
+	{
+		current->prev->next = NULL, free(current);
+		return (1);
+	}
+	return (-1);
+}
