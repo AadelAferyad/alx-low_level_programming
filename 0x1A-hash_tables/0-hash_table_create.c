7d7486ac798eb1769c3dabae50e81a179927c296
AadelAferyad <animevineu@gmail.com>
2023-12-21 19:14:40 +0100

    create hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,11 +12,25 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash = NULL;
+	unsigned long int i = 0;
 
 	if (!size)
 		return (NULL);
-	hash = malloc(size);
+	hash = malloc(sizeof(hash_table_t));
 	if (!hash)
 		return (NULL);
+	hash->size = size;
+	hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (!hash->array)
+	{
+		free(hash);
+		return (NULL);
+	}
+	while (i <= size)
+	{
+		hash->array[i] = NULL;
+		i++;
+	}
+
 	return (hash);
 }
