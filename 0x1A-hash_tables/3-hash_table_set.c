f4a22789824c10c373a4cea91982fd0e35b56bfc
AadelAferyad <animevineu@gmail.com>
2023-12-21 22:57:28 +0100

    not sure what to do with node next

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,75 @@
+#include "hash_tables.h"
+
+
+/**
+ * function_betty - ffunction helper for betty.
+ * @node: node.
+ * @key: the hash key.
+ * @value: the value hash code will take.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+
+int function_betty(hash_node_t *node, const char *key, const char *value)
+{
+	if (!node)
+		return (0);
+	node->key = strdup(key);
+	if (!(node->key))
+		return (0);
+	node->value = strdup(value);
+	if (!(node->value))
+	{
+		free(node->key);
+		return (0);
+	}
+	return (1);
+}
+
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: the hash table.
+ * @key: the hash key.
+ * @value: the value hash code will take.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0, i = 0;
+	hash_node_t *node = NULL;
+
+	if (!ht || !value)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = malloc(sizeof(hash_node_t));
+	if (function_betty(node, key, value) == 0)
+		return (0);
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = node;
+		if (index + 1 < ht->size)
+			ht->array[index]->next = ht->array[index + 1];
+		else
+			ht->array[index]->next = NULL;
+	}
+	else
+	{
+		while (i < ht->size)
+		{
+			if (ht->array[i] == NULL)
+			{
+				ht->array[i] = node;
+				if (i + 1 < ht->size)
+					ht->array[i]->next = ht->array[i + 1];
+				else
+					ht->array[i]->next = NULL;
+				break;
+			}
+			i++;
+		}
+	}
+	return (i < ht->size ? 1 : 0);
+}
