cac600ff8b39194e33538e09cf1461e2d3537d0c
AadelAferyad <animevineu@gmail.com>
2023-12-22 17:44:04 +0100

    printer

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,44 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ *
+ * @ht: the hash table.
+ *
+ */
+
+/*
+*[0]== -->0-->0-->0-->N
+*[1]== -->0-->N
+*[3]== -->0-->0-->N
+*[4]== -->0-->0-->0-->0-->N
+*[5]== -->0-->N
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *tmp = NULL;
+	int bol = 0;
+
+	if (!ht)
+	{
+		printf("{}\n");
+		return;
+	}
+	putchar('{');
+	while (index < ht->size)
+	{
+		tmp = ht->array[index];
+		while (tmp)
+		{
+			if (bol)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
+			bol = 1;
+		}
+		index++;
+	}
+	printf("}\n");
+}
