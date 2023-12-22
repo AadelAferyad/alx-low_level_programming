392db7d2b7572246a393a9a07483745f6ee75671
AadelAferyad <animevineu@gmail.com>
2023-12-22 16:21:45 +0100

    new patter unlocked new new

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,10 +10,17 @@
  * Return: 1 if it succeeded, 0 otherwise.
  */
 
+/*
+*0-->0-->0-->([0]== -->0)-->N
+*0-->([1]== -->0)-->N
+*0-->0-->0-->([2]== -->0)-->N
+*0-->-->([3]== -->0)-->N
+*0-->0-->0-->0-->([4]== -->0)-->N
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *node = NULL;
+	hash_node_t *node = NULL, *tmp = NULL;
 
 	if (!ht)
 		return (0);
@@ -23,22 +30,33 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
+	tmp = ht->array[index];
+	while (tmp)/*check for collision*/
+	{/*[1] = -->0-->0-->0*/
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value), tmp->value = strdup(value ? value : "");
+			if (!(tmp->value))
+			{
+				free(tmp->key), free(node);
+				return (0);
+			}
+			return (1);
+		}
+		tmp = tmp->next;
+	}
 	node->key = strdup(key);
 	if (!(node->key))
 	{
 		free(node);
 		return (0);
 	}
-	if (!value)
-		node->value = strdup("");
-	else
-		node->value = strdup(value);
+	node->value = strdup(value ? value : "");
 	if (!(node->value))
 	{
 		free(node->key), free(node);
 		return (0);
 	}
-	node->next = ht->array[index];
-	ht->array[index] = node;
+	node->next = ht->array[index], ht->array[index] = node;
 	return (1);
 }
