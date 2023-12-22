4e2201783afa53462d19d49a01ae70c83d8c5bdd
AadelAferyad <animevineu@gmail.com>
2023-12-22 18:31:18 +0100

    hide

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
+	if (index > ht->size)
 		return (0);
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
